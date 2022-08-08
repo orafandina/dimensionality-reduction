9152e57e2fd6f080d49d62f11e05441cbc3da063
orafandina <94124355+orafandina@users.noreply.github.com>
2022-08-08 13:51:36 +0200

    get random space, more efficient

--- Metric_Spaces.py
+++ Metric_Spaces.py
@@ -1,1 +1,16 @@
 
+""" Metric spaces: creating, testing for being valid, Euclidean, non-Euclidean, etc.  """
+
+import sys 
+import numpy as np
+from numpy import linalg as LA
+import math
+
+
+#Retruns a randomly generated vector space, of size and dimesnion dim, as numpy 2D array
+def get_random_space(size, dim):
+    space=np.random.randn(size, dim)
+    for i in range(size):
+        sdv=np.random.randint(1,30)
+        space[i]=sdv*space[i]
+    return(space);
