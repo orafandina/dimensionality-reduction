f1c135576751e9562fd65746ca6035c5ea045900
orafandina <94124355+orafandina@users.noreply.github.com>
2022-08-10 13:44:35 +0200

    Update Tests.py

--- Tests.py
+++ Tests.py
@@ -1,1 +1,33 @@
 
+
+"""
+Created on Wed August 10 13:15:37 2022
+
+@author: Ora Fandina
+"""
+import matplotlib.pyplot as plt
+
+#Classic embedding methods, to compare with
+from sklearn.random_projection import GaussianRandomProjection
+from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
+from sklearn.manifold import TSNE
+from sklearn.manifold import Isomap
+
+
+import numpy as np
+from numpy import linalg as LA
+import scipy
+import scipy.spatial
+import math
+import sys
+
+#convex opt package
+import cvxpy as cp
+
+#metric spaces and qaulit ymeasures of embedding methods
+import distortion_measures as dm
+import metric_spaces as ms
+
+
+
