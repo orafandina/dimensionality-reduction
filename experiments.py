b48e6ec749a3775a27c5f2513c6d72a84ab1ebc6
orafandina <94124355+orafandina@users.noreply.github.com>
2022-08-10 16:22:19 +0200

    new experiments

--- experiments.py
+++ experiments.py
@@ -6,6 +6,9 @@ Created on Wed August 10 13:15:37 2022
 @author: Ora Fandina
 """
 import matplotlib.pyplot as plt
+import sklearn
+
+sklearn.__file__
 
 #Classic embedding methods, to compare with
 from sklearn.random_projection import GaussianRandomProjection
@@ -28,7 +31,7 @@ import cvxpy as cp
 #metric spaces and qaulit ymeasures of embedding methods
 import distortion_measures as dm
 import metric_spaces as ms
-
+import approx_algo as AA
 
 
 """ Setting up various embeddings  """
@@ -68,36 +71,41 @@ def TSNE_transf(dists, k):
 #MDS;SMACOF
 
 """   
-Input: d is the size and dimesnion of the input metric space to generate.
-       It will be randomly generated (according to one of the schemes, defined by metric_type parameter), 
-       the MDS algorithm then is applied on the input space
-       
-       range_k - an array of dimesnions to embed into
-       
-       q - is the measure rank to compute the quality of the embedidng
-       
-       t - is the number of times to repreat the experiment. The result is the average over t.
-       
-      epsilon - is the error parameter, controlling closeness to Euclidean space, in get_random_epsilon_close_non_Eucl function
 
-Outputs average distortions of the embedding, for each of the given dimesnions in range_k
+Applies MDS embedidng om input_dists. embeds into dimesnions in range_k array. Computes distortion of rank q.
+
+Inputs: input_dist - distance matrix of an input metric space
+ 
+        range_k - n array of dimesnions to embed into 
+        
+        q - distortion rank 
+        
+        measure_type - a string from {'lq_dist', 'REM', 'sigma'}
 """ 
-def run_MDS_range_k(d, range_k, q, t, epsilon):
+
+def run_MDS_range_k(input_dists, range_k, q, measure_type):
     answer=np.zeros(range_k.shape)
     
-    for i in range(t):
-        original_dists, distorted_space=ms.get_random_epsilon_close_non_Eucl(d, epsilon)
-        distorts=[]
-        for i in range(range_k.size):
-            embedded=MDS_transf(distorted_space,range_k[i])
-            lq_value=dm.lq_dist(distorted_space, ms.space_to_dist(embedded),q)
-            distorts.append(lq_value)
-        lq_array=np.array(distorts)
-        answer=answer+lq_array
-    av_distorts=np.true_divide(answer,t)
-    print("The array of MDS lq-dists is:", av_distorts, "for the dimensions", range_k)
-    return(av_distorts);
-
+    dict={
+     'lq_dist': dm.lq_dist,
+     'REM': dm.REM_q,
+     'sigma': dm.sigma_q
+    }
+    
+    measure=dict[measure_type]
+    for i in len(range_k):
+        embedded=MDS_transf(input_dists,range_k[i])
+        answer[i]=measure(input_dists,ms.space_to_dist(embedded),q)
+    return(answer)    
+         
+        
+X=ms.get_random_space(10, 10) 
+dists_X=ms.space_to_dist(X)   
+print(dists_X)     
+    
+    
+        
+    
 
 
 #TSNE
@@ -106,7 +114,7 @@ def run_TSNE_range_k(d, range_k, q, t, epsilon):
     answer=np.zeros(range_k.shape)
     for i in range(t):
         #print("PCA Iteration t=",i)
-        original_dists, distorted_space=get_random_epsilon_far_non_Eucl(d, epsilon)
+        distorted_space=ms.get_random_epsilon_close_non_Eucl(d, epsilon)(d, epsilon)
         #print("euclidean metric space?", is_Euclidean_space(distorted_space))
         #X=get_random_space(d,d)
         #X=get_Normal_space(d,d)
@@ -118,7 +126,7 @@ def run_TSNE_range_k(d, range_k, q, t, epsilon):
             #print("The type of k is",type(k))
             embedded=TSNE_transf(distorted_space,range_k[i])
             #print("This step is done")
-            lq_value=lq_dist(distorted_space, space_to_dist(embedded),q)
+            lq_value=dm.lq_dist(distorted_space, ms.space_to_dist(embedded),q)
             distorts.append(lq_value)
         lq_array=np.array(distorts)
         answer=answer+lq_array
@@ -131,11 +139,11 @@ def run_TSNE_range_k(d, range_k, q, t, epsilon):
 def run_Approx_range_k_lqdist(d, range_k, q, T, epsilon):
     answer=np.zeros(range_k.shape)
     for i in range(T):
-        original_dists, distorted_space=get_random_epsilon_far_non_Eucl(d, epsilon)
+        distorted_space=ms.get_random_epsilon_close_non_Eucl(d, epsilon)(d, epsilon)
         distorts=[]
         for k in np.nditer(range_k):
-            embedded=Approx_Algo(distorted_space, k, q)
-            lq_value=lq_dist(distorted_space, space_to_dist(embedded),q)
+            embedded=AA.Approx_Algo(distorted_space, k, q)
+            lq_value=dm.lq_dist(distorted_space, ms.space_to_dist(embedded),q)
             distorts.append(lq_value)
         lq_array=np.array(distorts)
         answer=answer+lq_array
